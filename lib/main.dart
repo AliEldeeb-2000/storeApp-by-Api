a7eab45d6ad00db1d4c83ce1099a5714e185cf4a
AliEldeeb <74424030+AliEldeeb-2000@users.noreply.github.com>
2023-01-13 23:10:58 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:store/screens/home_page.dart';
 import 'package:store/screens/update_product_page.dart';
 
-import 'Size_Config.dart';
-import 'constants.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
